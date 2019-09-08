c8303cfc69d4095adedcae5014f8ff5309e45f4a
ShinNoNoir <shinnonoir+git@gmail.com>
2019-09-08 14:37:54 +0200

    Add ZeroSelectManyTest

--- VectorSpace.Tests/MonadLawTests.cs
+++ VectorSpace.Tests/MonadLawTests.cs
@@ -27,6 +27,25 @@ namespace VectorSpace.Tests
             );
         }
 
+        [Theory]
+        [MemberData(nameof(GetRandomVectorData))]
+        public void ZeroSelectManyTest<T>(Vector<T> v)
+        {
+            // The first part of this test requires any arbitrary T -> V<S> function:
+            Func<T, Vector<T>> anyFunction = _ => v;
+
+            // 0.SelectMany(f) = 0
+            Assert.Equal(
+                Vector<T>.Zero.SelectMany(anyFunction),
+                Vector<T>.Zero
+            );
+
+            // v.SelectMany(_ => 0) = 0
+            Assert.Equal(
+                v.SelectMany(_ => Vector<T>.Zero),
+                Vector<T>.Zero
+            );
+        }
 
 
         private static Random Rand = new Random();
