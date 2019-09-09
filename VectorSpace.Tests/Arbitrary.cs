4ca7a8e7530567d73d092999fe2dfadd479dc472
ShinNoNoir <shinnonoir+git@gmail.com>
2019-09-09 19:35:52 +0200

    Move random vector generation to a new Arbitrary class

---
+++ VectorSpace.Tests/Arbitrary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorSpace.Tests
+{
+    public static class Arbitrary
+    {
+        private static Random Rand = new Random();
+        public static bool GetRandomBool() => Rand.Next(2) == 0 ? false : true;
+        public static double GetRandomScalar() => Rand.Next(-2, 10);
+
+
+        public static Vector<bool> GetRandomVector()
+        {
+            var res = new Vector<bool>();
+            var numComponents = Rand.Next(5);
+            for (var i = 0; i != numComponents; ++i)
+            {
+                res[GetRandomBool()] = GetRandomScalar();
+            }
+            return res;
+        }
+
+        public static Vector<Vector<bool>> GetRandomNestedVector()
+        {
+            var res = new Vector<Vector<bool>>();
+            var numComponents = Rand.Next(5);
+            for (var i = 0; i != numComponents; ++i)
+            {
+                res[GetRandomVector()] = GetRandomScalar();
+            }
+            return res;
+        }
+
+        public static Vector<Vector<Vector<bool>>> GetRandomNestedNestedVector()
+        {
+            var res = new Vector<Vector<Vector<bool>>>();
+            var numComponents = Rand.Next(5);
+            for (var i = 0; i != numComponents; ++i)
+            {
+                res[GetRandomNestedVector()] = GetRandomScalar();
+            }
+            return res;
+        }
+
+    }
+}
--- VectorSpace.Tests/MonadLawTests.cs
+++ VectorSpace.Tests/MonadLawTests.cs
@@ -70,48 +70,11 @@ namespace VectorSpace.Tests
 
 
 
-        private static Random Rand = new Random();
-        public static bool GetRandomBool() => Rand.Next(2) == 0 ? false : true;
-        public static double GetRandomScalar() => Rand.Next(-2, 10);
-        
-        public static Vector<bool> GetRandomVector()
-        {
-            var res = new Vector<bool>();
-            var numComponents = Rand.Next(5);
-            for (var i=0; i != numComponents; ++i)
-            {
-                res[GetRandomBool()] = GetRandomScalar();
-            }
-            return res;
-        }
-
-        public static Vector<Vector<bool>> GetRandomNestedVector()
-        {
-            var res = new Vector<Vector<bool>>();
-            var numComponents = Rand.Next(5);
-            for (var i = 0; i != numComponents; ++i)
-            {
-                res[GetRandomVector()] = GetRandomScalar();
-            }
-            return res;
-        }
-
-        public static Vector<Vector<Vector<bool>>> GetRandomNestedNestedVector()
-        {
-            var res = new Vector<Vector<Vector<bool>>>();
-            var numComponents = Rand.Next(5);
-            for (var i = 0; i != numComponents; ++i)
-            {
-                res[GetRandomNestedVector()] = GetRandomScalar();
-            }
-            return res;
-        }
-
         public static IEnumerable<object[]> GetRandomVectorData()
-            => Enumerable.Range(0, 100).Select(_ => new object[] { GetRandomVector() });
+            => Enumerable.Range(0, 100).Select(_ => new object[] { Arbitrary.GetRandomVector() });
 
         public static IEnumerable<object[]> GetRandomNestedNestedVectorData()
-            => Enumerable.Range(0, 100).Select(_ => new object[] { GetRandomNestedNestedVector() });
+            => Enumerable.Range(0, 100).Select(_ => new object[] { Arbitrary.GetRandomNestedNestedVector() });
 
     }
 }
